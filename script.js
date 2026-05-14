6cf2674610c4563ebaeb9edcfc3a27160ca27ec9
Fedor Andreev <165912633+EnainRoflan@users.noreply.github.com>
2026-05-14 20:25:09 +0500

    ывкаепнгрошщлздх
    ячсмитьбю.

--- script.js
+++ script.js
@@ -74,3 +74,21 @@ btnHome.addEventListener('click', () => {
     screenResult.classList.add('hidden');
     screenStart.classList.remove('hidden');
 });
+
+let block = document.querySelector('.memMenu')
+document.addEventListener('mousemove', function(e) {
+    let dx = e.pageX - window.innerWidth / 2
+    let dy = e.pageY - window.innerHeight / 2
+    let angleX = 20 * dx / window.innerWidth / 2
+    let angleY = 20 * dy / window.innerHeight / 2
+    block.style.transform = `rotateX(${-angleY}deg) rotateY(${angleX}deg)`
+})
+
+let block2 = document.querySelector('.result')
+document.addEventListener('mousemove', function(e) {
+    let dx = e.pageX - window.innerWidth / 2
+    let dy = e.pageY - window.innerHeight / 2
+    let angleX = 20 * dx / window.innerWidth / 2
+    let angleY = 20 * dy / window.innerHeight / 2
+    block2.style.transform = `rotateX(${-angleY}deg) rotateY(${angleX}deg)`
+})
